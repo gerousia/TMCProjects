cff6d2834c298a58de01b3380a091531e54d7155
Gee <111046706+ginomari@users.noreply.github.com>
2023-06-23 15:22:10 +0800

    Renamed Folders

--- mooc-java-programming-i/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.Scanner;
-
-public class AverageOfPositiveNumbers {
-
-    public static void main(String[] args) {
-        new UserInterface(new Scanner(System.in)).start();
-    }
-}
